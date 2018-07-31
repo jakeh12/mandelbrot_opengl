6c5eec2207e07385c74bd1ca132ac0deefa0cfc6
Jakub Hladik <jhladik@iastate.edu>
2018-07-30 22:18:55 -0500

    Initial Commit

---
+++ mandelbrot_opengl/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mandelbrot_opengl
+//
+//  Created by Jakub Hladik on 7/30/18.
+//  Copyright © 2018 Jakub Hladik. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
